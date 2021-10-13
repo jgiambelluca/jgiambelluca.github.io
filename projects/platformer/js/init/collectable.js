8e886f89fa038e50604c8c8313eaf15e42d4aa88
jgiambelluca <jgg2666@ppsbstudent.org>
2021-10-13 18:41:44 +0000

    platformer

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -38,7 +38,9 @@
         
         // example: 
         createCollectable(type.steve, 200, 170, 6, 0.7);
-        
+        createCollectable(type.steve, 100, 200, 6, 0.7);
+        createCollectable(type.steve, 500, 170, 6, 0.7);
+
         
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
